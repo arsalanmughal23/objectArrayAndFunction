bb74d6abb07ca4734b4b77166255867503ba7c21
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-07-07 19:24:35 +0500

    Initial Commit

--- arrayManipulation_RearrangingWords/index.js
+++ arrayManipulation_RearrangingWords/index.js
@@ -1,14 +1,5 @@
 "use strict";
 var _a;
 const scrambledArray = ["student", "of", true, 123, "am", "a", "GIAIC", "I"];
-// given below code delete 4 Element from 2 index of scrambledArray variable by using splice method
-// Splice returns deleted elements
-// Then i carry out the slice of last 2 elements from the result of splice method
-// And store result in scrapeWords variable
-let scrapeWords = scrambledArray.splice(2, 4).slice(-2);
-// console.log(scrapeWords)
-// Now, I am pop-out the last element from scrambledArray variable
-// And add poped-out element with scrapeWords elements on starting index of scrambledArray variable
-scrambledArray.unshift((_a = scrambledArray.pop()) !== null && _a !== void 0 ? _a : '', ...scrapeWords);
-// console.log(scrambledArray)
+scrambledArray.unshift((_a = scrambledArray.pop()) !== null && _a !== void 0 ? _a : '', ...scrambledArray.splice(2, 4).slice(-2));
 console.log(scrambledArray.join(' '));
--- arrayManipulation_RearrangingWords/index.ts
+++ arrayManipulation_RearrangingWords/index.ts
@@ -1,15 +1,19 @@
+// const scrambledArray = ["student", "of",true, 123, "am", "a", "GIAIC", "I"];
+
+// // given below code delete 4 Element from 2 index of scrambledArray variable by using splice method
+// // Splice returns deleted elements
+// // Then i carry out the slice of last 2 elements from the result of splice method
+// // And store result in scrapeWords variable
+// let scrapeWords = scrambledArray.splice(2,4).slice(-2);
+// // console.log(scrapeWords)
+
+// // Now, I am pop-out the last element from scrambledArray variable
+// // And add poped-out element with scrapeWords elements on starting index of scrambledArray variable
+// scrambledArray.unshift(scrambledArray.pop() ?? '', ...scrapeWords);
+// // console.log(scrambledArray)
+
+// console.log(scrambledArray.join(' '));
+
 const scrambledArray = ["student", "of",true, 123, "am", "a", "GIAIC", "I"];
-
-// given below code delete 4 Element from 2 index of scrambledArray variable by using splice method
-// Splice returns deleted elements
-// Then i carry out the slice of last 2 elements from the result of splice method
-// And store result in scrapeWords variable
-let scrapeWords = scrambledArray.splice(2,4).slice(-2);
-// console.log(scrapeWords)
-
-// Now, I am pop-out the last element from scrambledArray variable
-// And add poped-out element with scrapeWords elements on starting index of scrambledArray variable
-scrambledArray.unshift(scrambledArray.pop() ?? '', ...scrapeWords);
-// console.log(scrambledArray)
-
+scrambledArray.unshift(scrambledArray.pop() ?? '', ...scrambledArray.splice(2,4).slice(-2));
 console.log(scrambledArray.join(' '));
